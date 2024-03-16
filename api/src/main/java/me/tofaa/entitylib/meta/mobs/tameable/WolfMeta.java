13e29016ab7790d8ceebefdfb0b1cfe4b03aa6a6
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2024-03-16 21:14:32 +0400

    fix wolf collar color

--- api/src/main/java/me/tofaa/entitylib/meta/mobs/tameable/WolfMeta.java
+++ api/src/main/java/me/tofaa/entitylib/meta/mobs/tameable/WolfMeta.java
@@ -1,6 +1,7 @@
 package me.tofaa.entitylib.meta.mobs.tameable;
 
 import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
+import me.tofaa.entitylib.extras.DyeColor;
 import me.tofaa.entitylib.meta.Metadata;
 import me.tofaa.entitylib.meta.types.TameableMeta;
 
@@ -27,6 +28,14 @@ public class WolfMeta extends TameableMeta {
 
     public void setCollarColor(int value) {
         super.metadata.setIndex(offset(OFFSET, 1), EntityDataTypes.INT, value);
+    }
+
+    public DyeColor getCollarColorAsDye() {
+        return DyeColor.values()[super.metadata.getIndex(offset(OFFSET, 1), DyeColor.RED.ordinal())];
+    }
+
+    public void setCollarColor(DyeColor color) {
+        super.metadata.setIndex(offset(OFFSET, 1), EntityDataTypes.INT, color.ordinal());
     }
 
     public int getAngerTime() {
