bf191048ccc778cedcf0cd57c3fa17ca0b440ae3
xDec0de_ <xdec0deyt@gmail.com>
2024-07-04 18:22:10 +0200

    Add Bogged & Breeze meta (Base only)

---
+++ api/src/main/java/me/tofaa/entitylib/meta/mobs/monster/skeleton/BoggedMeta.java
@@ -0,0 +1,11 @@
+package me.tofaa.entitylib.meta.mobs.monster.skeleton;
+
+import me.tofaa.entitylib.meta.Metadata;
+
+public class BoggedMeta extends SkeletonMeta {
+
+    public BoggedMeta(int entityId, Metadata metadata) {
+        super(entityId, metadata);
+    }
+
+}
