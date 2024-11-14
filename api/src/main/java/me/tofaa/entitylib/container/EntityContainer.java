9e530ebed86fc1c427f1e420f90532ad0a3d781d
= <asdada@gmail.com>
2024-11-14 19:23:12 +0200

    Add EntityContainer#empty

--- api/src/main/java/me/tofaa/entitylib/container/EntityContainer.java
+++ api/src/main/java/me/tofaa/entitylib/container/EntityContainer.java
@@ -1,10 +1,10 @@
 package me.tofaa.entitylib.container;
 
 import me.tofaa.entitylib.wrapper.WrapperEntity;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.UUID;
+import java.util.*;
 
 
 /**
@@ -18,6 +18,46 @@ public interface EntityContainer extends Iterable<WrapperEntity> {
     static EntityContainer basic() {
         return new ContainerImpl();
     }
+
+    static EntityContainer empty() {
+        return new EntityContainer() {
+            @Override public void addEntity(WrapperEntity entity) {}
+            @Override public void removeEntity(WrapperEntity entity, boolean despawn) {}
+            @Override public void removeEntity(int entityId, boolean despawn) {}
+            @Override public void removeEntity(UUID uuid, boolean despawn) {}
+            @Override public void clearEntities(boolean despawn) {}
+            @Override public void tick() {}
+            @Override public Collection<WrapperEntity> getEntities() {return Collections.emptyList();}
+            @Override
+            public @Nullable WrapperEntity getEntity(UUID uuid) {
+                return null;
+            }
+            @Override
+            public @Nullable WrapperEntity getEntity(int entityId) {
+                return null;
+            }
+            @Override
+            public boolean containsEntity(UUID uuid) {
+                return false;
+            }
+
+            @Override
+            public boolean containsEntity(int entityId) {
+                return false;
+            }
+
+            @Override
+            public boolean containsEntity(WrapperEntity entity) {
+                return false;
+            }
+
+            @Override
+            public @NotNull Iterator<WrapperEntity> iterator() {
+                return getEntities().iterator();
+            }
+        };
+    }
+
 
     void addEntity(WrapperEntity entity);
 
