7d3dbbf2dc4317e9c50fea0b6508f991d0a9e6e2
Bram <bram@bramdekker.com>
2026-01-20 14:12:29 +0100

    Updated: Gradle Wrapper from 9.2.1 -> 9.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import java.io.ByteArrayOutputStream
-
 val fullVersion = "3.0.3"
 val snapshot = true
 
@@ -27,11 +25,6 @@ ext["versionBeta"] = getVersionMeta(false)
 ext["versionNoHash"] = "$fullVersion${getVersionMeta(false)}"
 
 tasks {
-    wrapper {
-        gradleVersion = "8.8"
-        distributionType = Wrapper.DistributionType.ALL
-    }
-
     fun subModuleTasks(taskName: String): List<Task> {
         return subprojects
             .filter { it.name != "platforms" }
